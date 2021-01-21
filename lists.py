cedfad95bd4e53c06179559d3c53b91854c61311
rysamuel9 <ronysamuel19@gmail.com>
2021-01-21 22:56:01 +0700

    list & set

---
+++ lists.py
@@ -0,0 +1,12 @@
+#Define a list of names
+
+names = ["Rony", "Samuel", "Tamba", "Rosam"]
+
+# n#ames.pop()
+# names.append("Tambul")
+# names.insert(1, "Santuy")
+# names.sort()
+# names.reverse()
+names.count(1)
+
+print(names)
