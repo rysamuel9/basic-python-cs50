01a52f5eb05ab48987c3900c11889208eb1cc478
rysamuel9 <ronysamuel19@gmail.com>
2021-01-21 23:51:16 +0700

    dict, func, loops

---
+++ loops.py
@@ -0,0 +1,7 @@
+# for i in range(999):
+#     i += 1
+#     print(i)
+
+names = "Samuel"
+for character in names:
+    print(i)
---
+++ squares.py
@@ -0,0 +1,5 @@
+from functions import square()
+
+for i in range(10):
+    i += 1
+    print(f"The square of {i} is {square(i)}")
